dbd37fdca2963428a972bdd32cb56d7eaca91915
Nourhan <nourhan.saeed@almatar.com>
2025-07-19 21:30:20 +0300

    update db

--- src/modules/note/note.controller.js
+++ src/modules/note/note.controller.js
@@ -1,49 +1,35 @@
-import { noteModel } from "../../../db/model/note.model.js"
-import catchError from "../../middleware/catchError.js"
+import { noteModel } from "../../../db/model/note.model.js";
+import catchError from "../../middleware/catchError.js";
 
+const addNote = catchError(async (req, res) => {
+  let addedNote = await noteModel.insertMany(req.body);
+  res.json({ message: "Hi", addedNote });
+});
 
-const addNote = catchError(async(req,res) => {
+const getNote = catchError(async (req, res) => {
+  let notes = await noteModel
+    .find({ createdBy: req.params.id })
+    .populate("createdBy");
+  res.json({ message: "Hi", notes });
+});
 
-    req.body.createdBy = req.user._id
-    let addedNote = await noteModel.insertMany(req.body)
+const getAllNote = catchError(async (req, res) => {
+  let notes = await noteModel.populate("createdBy");
+  res.json({ message: "Hi", notes });
+});
 
-    res.json({message:"Hi",addedNote})
-})
+const updateNote = catchError(async (req, res) => {
+  let notes = await noteModel.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+  });
+  res.json({ message: "Hi", notes });
+});
 
-
-// find ... []
-// findOne ... {} ... null
-// findId
-const getNote = catchError(async(req,res) => {
-    
-    let notes =  await noteModel.find({createdBy: "66914e393496ec9cb10ad158"}).populate("createdBy")
-     res.json({message:"Hi",notes})
- })
-
-// findByIdAndUpdate ... {}
-// findOneAndUpdate ... {}
-// findOneAndReplace ... {}
-
-
-// updateOne
-// updateMany
-// replaceOne
-const updateNote =catchError( async(req,res) => {
-    let notes =  await noteModel.findByIdAndUpdate(req.params.id, {title:req.body.title})
-     res.json({message:"Hi",notes})
- })
- 
-
-
- const deleteNote = catchError(async(req,res) => {
-
-    let note =  await noteModel.findByIdAndDelete({_id:req.params.id,createdBy: req.user._id})
-    res.json({message:"Hi",note})
-
- })
-export {
-    addNote,
-    getNote,
-    updateNote,
-    deleteNote
-}
+const deleteNote = catchError(async (req, res) => {
+  let note = await noteModel.findByIdAndDelete({
+    _id: req.params.id,
+    createdBy: req.params.userId,
+  });
+  res.json({ message: "Hi", note });
+});
+export { addNote, getNote, updateNote, deleteNote, getAllNote };
--- src/modules/note/note.routes.js
+++ src/modules/note/note.routes.js
@@ -7,9 +7,10 @@ const noteRoutes = express.Router();
 // noteRoutes.use("/note",verifyToken)
 noteRoutes.post("/note", addNote);
 noteRoutes.get("/note", getNote);
+noteRoutes.get("/note/:id", getNote);
 
 noteRoutes.put("/note/:id", updateNote);
 
-noteRoutes.delete("/note/:id", deleteNote);
+noteRoutes.delete("/note/:id/:userId", deleteNote);
 
 export default noteRoutes;
