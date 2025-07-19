f945af36208127846ecc3c3e0e922e39bc4cde78
Nourhan <nourhan.saeed@almatar.com>
2025-07-19 21:18:33 +0300

    update db

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -31,7 +31,7 @@ const signIn = catchError(async (req, res, next) => {
   // if(foundedUser.isConfirmed == false)
   //     return res.status(401).json({message:"U should verify ur account"})
 
-  const token = jwt.sign({ email }, "Note");
+  const token = jwt.sign({ email: req.body.email }, "Note");
   if (foundedUser.password == req.body.password) {
     res.json({ message: "welocme", token });
   } else {
