03fd1937cea3c0fcbc518ef4f1df9164458161cf
Nourhan <nourhan.saeed@almatar.com>
2025-07-20 00:42:09 +0300

    update db

--- src/modules/note/note.controller.js
+++ src/modules/note/note.controller.js
@@ -3,19 +3,19 @@ import catchError from "../../middleware/catchError.js";
 
 const addNote = catchError(async (req, res) => {
   let addedNote = await noteModel.insertMany(req.body);
-  res.json({ message: "Hi", addedNote });
+  res.json({ message: "success", addedNote });
 });
 
 const getNote = catchError(async (req, res) => {
   console.log(req.params.id);
 
   let notes = await noteModel.findById(req.params.id);
-  res.json({ message: "Hi", notes });
+  res.json({ message: "success", notes });
 });
 
 const getAllNote = catchError(async (req, res) => {
   let notes = await noteModel.find();
-  res.json({ message: "Hi", notes });
+  res.json({ message: "success", notes });
 });
 
 const updateNote = catchError(async (req, res) => {
@@ -26,7 +26,7 @@ const updateNote = catchError(async (req, res) => {
     },
     { new: true }
   );
-  res.json({ message: "Hi", notes });
+  res.json({ message: "success", notes });
 });
 
 const deleteNote = catchError(async (req, res) => {
@@ -34,6 +34,6 @@ const deleteNote = catchError(async (req, res) => {
     _id: req.params.id,
     createdBy: req.params.userId,
   });
-  res.json({ message: "Hi", note });
+  res.json({ message: "success", note });
 });
 export { addNote, getNote, updateNote, deleteNote, getAllNote };
