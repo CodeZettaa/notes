79b3935a4214ce0b659c6c6910706052029a1d0e
Nourhan <nourhan.saeed@almatar.com>
2025-07-19 21:33:49 +0300

    update db

--- src/modules/note/note.routes.js
+++ src/modules/note/note.routes.js
@@ -1,12 +1,18 @@
 import express from "express";
-import { addNote, getNote, updateNote, deleteNote } from "./note.controller.js";
+import {
+  addNote,
+  getNote,
+  updateNote,
+  deleteNote,
+  getAllNote,
+} from "./note.controller.js";
 import { verifyToken } from "../../middleware/verifyToken.js";
 
 const noteRoutes = express.Router();
 
 // noteRoutes.use("/note",verifyToken)
 noteRoutes.post("/note", addNote);
-noteRoutes.get("/note", getNote);
+noteRoutes.get("/note", getAllNote);
 noteRoutes.get("/note/:id", getNote);
 
 noteRoutes.put("/note/:id", updateNote);
