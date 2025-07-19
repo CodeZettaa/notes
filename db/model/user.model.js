e452c28cb0505c119117943f55d9313b4e521127
Nourhan <nourhan.saeed@almatar.com>
2025-07-19 21:07:10 +0300

    update db

--- db/model/user.model.js
+++ db/model/user.model.js
@@ -1,38 +1,36 @@
-
 import mongoose from "mongoose";
 
-
-
-const userSchema = new mongoose.Schema({
-    name:String,
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
     email: {
-        type:String
+      type: String,
     },
     password: String,
     age: {
-        type:Number,
-        min:5
+      type: Number,
+      min: 5,
     },
-    isConfirmed : {
-        type: Boolean,
-        default: false
+    isConfirmed: {
+      type: Boolean,
+      default: false,
     },
     role: {
-        type: String,
-        enum: ['admin','user'],
-        default: 'user'
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
     },
-    otpCode:String,
-    otpExpire: Date
-} , {
+    otpCode: String,
+    otpExpire: Date,
+  },
+  {
     timestamps: true,
-    versionKey: false
-});
+    versionKey: false,
+  }
+);
 
-const userModel = mongoose.model("User", userSchema);
+const userModel = mongoose.model("Client", userSchema);
 
 export default userModel;
-
-
 
 // signup ....... send email  .... button verfiy ..... isConfirmed : true ... login ..bravo
